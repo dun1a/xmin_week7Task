33b0567090f599847ad650345dc3b3e6feceef5f
donya <donyaa@metropolia.fi>
2026-03-11 14:21:05 +0200

    edit jenkins and pom

--- src/main/test/java/CalculatorTest.java
+++ src/main/test/java/CalculatorTest.java
@@ -1,15 +1,15 @@
-
+import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void calculate() {
         String result = Calculator.calculate("10", "5");
         String expected = "Sum: 15.0, Subtract: 5.0, Divide: 2.0, Product: 50.0";
         org.junit.jupiter.api.Assertions.assertEquals(expected, result);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void calculate2() {
         String result = Calculator.calculate("20", "2");
         String expected = "Sum: 22.0, Subtract: 18.0, Divide: 10.0, Product: 40.0";
